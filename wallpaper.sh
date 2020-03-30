2938bac69e3d4d7e6965794588a0fc838cde10a8
xaviergregor <xavier@gregor.fr>
2020-03-30 09:09:20 +0200

    🖥j Wallpaper

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-mkdir -p ~/Pictures/Wallpapers/unsplash
-rm -f ~/Pictures/Wallpapers/unsplash/"$(date +%F)".png
-curl -s -L -o ~/Pictures/"$(date +%F)".png "https://source.unsplash.com/random/1920x1200/?hacker"  > /dev/null
-osascript -e "tell application \"System Events\" to set picture of every desktop to \"~/Pictures/Wallpapers/unsplash/$(date +%F).png\""
+
+# Creation du repertoire
+mkdir -p ~/Pictures/Wallpapers
+
+# Effacer le wallpaper du jour (option)
+rm -f ~/Pictures/Wallpapers/"$(date +%F)".png
+
+# Telechargement du wallpaper
+curl -s -L -o ~/Pictures/Wallpapers/"$(date +%F)".png "https://source.unsplash.com/random/1920x1200/?hacker"  > /dev/null
+
+# Utiliser le wallpaper comme fond d'ecran par defaut
+osascript -e "tell application \"System Events\" to set picture of every desktop to \"~/Pictures/Wallpapers/$(date +%F).png\""
+
+# On relance
 killall Dock
